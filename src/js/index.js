a10930fb2fc03a8056f2afa193d7a079abce8848
Yahya Kamaneh <kamaneh.yahya@gmail.com>
2023-12-05 22:57:10 +0330

    Add some changes to hero section

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// "use strict";
+//
+// const airplaneEl = document.querySelector(".airplane");
+// airplaneEl.style.left = `${
+//   window.screen.width + airplaneEl.getBoundingClientRect().width
+// }px`;
+// setInterval(() => {
+//   if (
+//     airplaneEl.getBoundingClientRect().left <=
+//     -airplaneEl.getBoundingClientRect().width
+//   ) {
+//     airplaneEl.style.visibility = "hidden";
+//     airplaneEl.style.left = `${
+//       window.screen.width + airplaneEl.getBoundingClientRect().width
+//     }px`;
+//     setTimeout(() => (airplaneEl.style.visibility = "visible"), 100);
+//   } else
+//     airplaneEl.style.left = `${
+//       +airplaneEl.getBoundingClientRect().left - 10
+//     }px`;
+// }, 100);
